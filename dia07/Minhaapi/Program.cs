d42585a46abf8807dace05281b537c02dc383d2c
alessandraasantos <alessandracosmo55@gmail.com>
2026-02-11 16:52:25 -0300

    incluindo chamada do redis no program

--- dia07/Minhaapi/Program.cs
+++ dia07/Minhaapi/Program.cs
@@ -1,11 +1,5 @@
+
 using Microsoft.EntityFrameworkCore;
-using MinhaApi.Data; 
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddOpenApi();
-
-// Registro do DbCusing Microsoft.EntityFrameworkCore;
 using MinhaApi.Data;
 using StackExchange.Redis; // ← ADICIONADO
 
@@ -28,35 +22,6 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
     var configuration = builder.Configuration["Redis:ConnectionString"] ?? "localhost:6379";
     return ConnectionMultiplexer.Connect(configuration);
 });
-
-// Recomendação do Npgsql para compatibilidade de timestamp (se aplicável)
-AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-
-builder.Services.AddControllers();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-// Mapear controllers
-app.MapControllers();
-
-app.Run();
-criontext com Npgsql
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    var cs = "Host=localhost;Port=5432;Database=minhaapi_db;Username=postgres;Password=postgres"; 
-    options
-        .UseNpgsql(cs)
-        .UseSnakeCaseNamingConvention();
-});
-
 
 // Recomendação do Npgsql para compatibilidade de timestamp (se aplicável)
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
